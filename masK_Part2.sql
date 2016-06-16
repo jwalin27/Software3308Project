3c21e67d657f17053634d18672c6479cc8f0935f
Bent.King <Ben.Tyler.King@gmail.com>
2016-06-15 22:51:40 -0600

    SQL file generated with mysqldump

---
+++ masK_Part2.sql
@@ -0,0 +1,127 @@
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: masK
+-- ------------------------------------------------------
+-- Server version	5.5.49-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Followers`
+--
+
+DROP TABLE IF EXISTS `Followers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Followers` (
+  `Key` int(3) NOT NULL AUTO_INCREMENT,
+  `UserID` int(3) NOT NULL,
+  `Follower` int(3) NOT NULL,
+  PRIMARY KEY (`Key`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Followers`
+--
+
+LOCK TABLES `Followers` WRITE;
+/*!40000 ALTER TABLE `Followers` DISABLE KEYS */;
+INSERT INTO `Followers` VALUES (1,1,4),(2,1,3),(3,5,1),(4,4,1),(5,1,2),(6,5,2),(7,3,4),(8,4,3),(9,2,5);
+/*!40000 ALTER TABLE `Followers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Login`
+--
+
+DROP TABLE IF EXISTS `Login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Login` (
+  `UserID` int(3) NOT NULL AUTO_INCREMENT,
+  `User` varchar(15) NOT NULL,
+  `Pass` varchar(16) NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Login`
+--
+
+LOCK TABLES `Login` WRITE;
+/*!40000 ALTER TABLE `Login` DISABLE KEYS */;
+INSERT INTO `Login` VALUES (1,'Super User','password'),(2,'OldAbe','pr0clamat10n'),(3,'sideofmustard','moo72'),(4,'nineteenfifty5','334193'),(5,'babeRuth49','mlbSquared11');
+/*!40000 ALTER TABLE `Login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Posts`
+--
+
+DROP TABLE IF EXISTS `Posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Posts` (
+  `PostID` int(6) NOT NULL AUTO_INCREMENT,
+  `UserID` int(3) NOT NULL,
+  `Post` varchar(175) NOT NULL,
+  PRIMARY KEY (`PostID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Posts`
+--
+
+LOCK TABLES `Posts` WRITE;
+/*!40000 ALTER TABLE `Posts` DISABLE KEYS */;
+INSERT INTO `Posts` VALUES (1,1,'Today I...'),(2,2,'Four Score...'),(3,2,'America will...'),(4,5,'The other night...'),(5,4,'Back in my day...');
+/*!40000 ALTER TABLE `Posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Profile`
+--
+
+DROP TABLE IF EXISTS `Profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Profile` (
+  `UserID` int(3) NOT NULL AUTO_INCREMENT,
+  `Bio` varchar(40) NOT NULL,
+  `Pic` varbinary(3000) DEFAULT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Profile`
+--
+
+LOCK TABLES `Profile` WRITE;
+/*!40000 ALTER TABLE `Profile` DISABLE KEYS */;
+INSERT INTO `Profile` VALUES (1,'The Best...',NULL),(2,'Born and Raised in...','http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTIwNjA4NjMzODg2NTc0MDky.jpg'),(3,'Just a humble...','https://mcdonalds.com.au/sites/mcdonalds.com.au/files/hero_pdt_mustard.png'),(4,'When I was born...',NULL),(5,'One of the best...',NULL);
+/*!40000 ALTER TABLE `Profile` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-15 22:50:19
