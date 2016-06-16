01f08bdc7c3f596a24cb3e70766588401d6d1bba
Bent.King <Ben.Tyler.King@gmail.com>
2016-06-15 21:07:22 -0600

    Initial script for creating first table and data - JLB

---
+++ masktable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS 'Login' (
+'UserID' int(1) NOT NULL auto_increment,
+'User' varchar(6) NOT NULL,
+'Pass' varchar(4) NOT NULL,
+PRIMARY KEY ('UserID')
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+INSERT INTO 'Login' ('UserID' 'User', 'Pass`' VALUES
+(1, 'Super User' 'password'),
+(2, 'OldAbe', 'pr0clamat10n'),
+(3, 'sideofmustard', 'moo72'),
+(4, 'nineteensixty5', '334193' ),
+(5, 'babeRuth49', 'mlbSquared11');
+
